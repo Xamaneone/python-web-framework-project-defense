6e2a92b03b6a055557c6720c48e02601a3ed7d9a
HOOLIGANS 801 <kristiyankan4ev@abv.bg>
2021-08-09 14:30:03 +0300

    added admin models

--- python_web_framework_project_defense/python_web_framework_project_defense/app_game_reviews/admin.py
+++ python_web_framework_project_defense/python_web_framework_project_defense/app_game_reviews/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from python_web_framework_project_defense.app_game_reviews.models import Review, Comment
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- python_web_framework_project_defense/python_web_framework_project_defense/profiles/admin.py
+++ python_web_framework_project_defense/python_web_framework_project_defense/profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from python_web_framework_project_defense.profiles.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
